ada5df94b0852530837f1cb711e27029adaff101
Draden Gaffney <dsg2dm@virginia.edu>
2018-07-22 17:44:12 -0700

    more test

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -6,10 +6,10 @@ const {Todo} = require('./../models/todo');
 const {ObjectId} = require('mongodb');
 
 const todos = [{
-  _id: new ObjectID(),
+  _id: new ObjectId(),
   text: 'first test'
 }, {
-  _id: new ObjectID(),
+  _id: new ObjectId(),
   text: 'send todo'
 }];
 
@@ -67,7 +67,7 @@ describe('Get /todos', () => {
       .expect(200)
       .expect((res) => {
         expect(res.body.todos.length).toBe(2);
-      });
+      })
       .end(done);
   });
 });
@@ -96,5 +96,46 @@ describe('GET /todos/:id', () => {
       .get(`/todos/123abc`)
       .expect(404)
       .end(done)
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+
+      });
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return a 404 if object id is invalid', (done) => {
+
+    request(app)
+      .delete(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+
   });
 });
