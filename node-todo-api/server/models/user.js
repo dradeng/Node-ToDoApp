c5bfe7d9854cec974cf4c2c4a769c82a2278ca9b
Draden Gaffney <dsg2dm@virginia.edu>
2018-07-24 10:00:55 -0700

    new middleware

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+
+
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -31,7 +33,7 @@ var UserSchema = new mongoose.Schema({
     }
   }]
 });
-UserSchema.methds.toJSON = function () {
+UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
 
@@ -47,6 +49,23 @@ UserSchema.methods.generateAuthToken = function () {
 
   return user.save().then(() => {
     return token;
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+  try {
+    decoded = jwt.verify(token, 'secretvar');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   });
 };
 
